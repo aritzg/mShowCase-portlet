cdac956d6070216d934fc961dafbdd2eb9cde0cf
aritzg <aritz@sareweb.net>
2011-12-09 10:12:20 +0100

    saved

--- docroot/WEB-INF/src/net/sareweb/mshowcase/portlets/p002/CategoryNavigatiorPortlet.java
+++ docroot/WEB-INF/src/net/sareweb/mshowcase/portlets/p002/CategoryNavigatiorPortlet.java
@@ -9,9 +9,12 @@ import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
 import javax.xml.namespace.QName;
 
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.servlet.SessionMessages;
 import com.liferay.portal.kernel.util.ParamUtil;
 
+import net.sareweb.mshowcase.model.Category;
 import net.sareweb.mshowcase.portlets.GenericMSCPortlet;
 import net.sareweb.mshowcase.service.CategoryLocalServiceUtil;
 
@@ -23,20 +26,77 @@ public class CategoryNavigatiorPortlet extends GenericMSCPortlet {
 
 		renderRequest.setAttribute(ATTR_CATEGORIES_0,
 				CategoryLocalServiceUtil.findCategoryByParentCategoryId(0));
+		getSubCategories(renderRequest);
+		setCurrentAndParentCategory(renderRequest);
 
 		super.doView(renderRequest, renderResponse);
+	}
+
+	private void setCurrentAndParentCategory(RenderRequest renderRequest) {
+		String catId = ParamUtil.get(renderRequest, PARAM_CATEGORY_ID, "0");
+		long categoryId = Long.parseLong(catId);
+		if (categoryId != 0) {
+			try {
+				Category cat = CategoryLocalServiceUtil.getCategory(categoryId);
+				if (cat.getParentCategoryId() == 0) {
+					renderRequest.setAttribute(ATTR_PARENT_CATEGORY_ID, catId);
+					renderRequest.setAttribute(ATTR_CATEGORY_ID, "0");
+				} else {
+					renderRequest.setAttribute(ATTR_PARENT_CATEGORY_ID,
+							String.valueOf(cat.getParentCategoryId()));
+					renderRequest.setAttribute(ATTR_CATEGORY_ID, catId);
+				}
+			} catch (Exception e) {
+				_log.error("Error getting category", e);
+			}
+			renderRequest.setAttribute(ATTR_CATEGORY_ID, catId);
+		} else {
+			renderRequest.setAttribute(ATTR_PARENT_CATEGORY_ID, "0");
+			renderRequest.setAttribute(ATTR_CATEGORY_ID, "0");
+		}
+	}
+
+	private void getSubCategories(RenderRequest renderRequest) {
+		String catId = ParamUtil.get(renderRequest, PARAM_CATEGORY_ID, "0");
+		long categoryId = Long.parseLong(catId);
+		if (categoryId != 0) {
+			try {
+				Category cat = CategoryLocalServiceUtil.getCategory(categoryId);
+				if (cat.getParentCategoryId() != 0) {
+					categoryId = cat.getParentCategoryId();
+				}
+				renderRequest.setAttribute(ATTR_CATEGORIES_1,
+						CategoryLocalServiceUtil
+								.findCategoryByParentCategoryId(categoryId));
+
+			} catch (Exception e) {
+				_log.error("Error getting category", e);
+			}
+		}
 	}
 
 	public void selectCategory(ActionRequest actionRequest,
 			ActionResponse actionResponse) {
 		javax.xml.namespace.QName qName = new QName("http://mshowcase.com",
 				"categoryId", "x");
-		SessionMessages.clear(actionRequest);
 		actionResponse.setEvent(qName,
 				ParamUtil.get(actionRequest, PARAM_CATEGORY_ID, "0"));
 	}
 
+	@javax.portlet.ProcessEvent(qname = "{http://mshowcase.com}categoryId")
+	public void handleProcessempinfoEvent(javax.portlet.EventRequest request,
+			javax.portlet.EventResponse response)
+			throws javax.portlet.PortletException, java.io.IOException {
+		javax.portlet.Event event = request.getEvent();
+		String value = (String) event.getValue();
+
+		response.setRenderParameter(PARAM_CATEGORY_ID, value);
+	}
+
+	public static final String ATTR_PARENT_CATEGORY_ID = "parentCategoryId";
+	public static final String ATTR_CATEGORY_ID = "categoryId";
 	public static final String ATTR_CATEGORIES_0 = "categories0";
+	public static final String ATTR_CATEGORIES_1 = "categories1";
 
 	public static final String PARAM_CATEGORY_ID = "categoryId";
 }
--- docroot/WEB-INF/src/net/sareweb/mshowcase/portlets/p003/InstanceListPortlet.java
+++ docroot/WEB-INF/src/net/sareweb/mshowcase/portlets/p003/InstanceListPortlet.java
@@ -1,23 +1,52 @@
 package net.sareweb.mshowcase.portlets.p003;
 
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import net.sareweb.mshowcase.model.Category;
+import net.sareweb.mshowcase.portlets.GenericMSCPortlet;
+import net.sareweb.mshowcase.service.CategoryLocalServiceUtil;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.util.bridges.mvc.MVCPortlet;
 
-/**
- * Portlet implementation class InstanceDetailPortlet
- */
-public class InstanceListPortlet extends MVCPortlet {
+public class InstanceListPortlet extends GenericMSCPortlet {
 
 	@Override
-	public void doView(javax.portlet.RenderRequest renderRequest,
-			javax.portlet.RenderResponse renderResponse)
-			throws java.io.IOException, javax.portlet.PortletException {
-
-		String categoryId = ParamUtil.get(renderRequest, PARAM_CATEGORY_ID, "0");
-		System.out.println("categoryId: " + categoryId);
+	public void doView(RenderRequest renderRequest,
+			RenderResponse renderResponse) throws java.io.IOException,
+			javax.portlet.PortletException {
+		
+		prepareCategories(renderRequest, renderResponse);
 		
 		super.doView(renderRequest, renderResponse);
-	};
+	}
+
+	private void prepareCategories(RenderRequest renderRequest,
+			RenderResponse renderResponse) {
+		String catId = ParamUtil
+				.get(renderRequest, PARAM_CATEGORY_ID, "0");
+		
+		long categoryId = Long.parseLong(catId);
+		
+		if(categoryId!=0){
+			try {
+				Category category = CategoryLocalServiceUtil.getCategory(categoryId);
+				if(category.getParentCategoryId()==0){
+					renderRequest.setAttribute(ATTR_CATEGORY_0, category);
+				}
+				else{
+					renderRequest.setAttribute(ATTR_CATEGORY_1, category);
+					Category category0 = CategoryLocalServiceUtil.getCategory(category.getParentCategoryId());
+					renderRequest.setAttribute(ATTR_CATEGORY_0, category0);
+				}
+			} catch (Exception e) {
+				_log.error("Error getting categories", e);
+			}
+		}
+	}
 
 	@javax.portlet.ProcessEvent(qname = "{http://mshowcase.com}categoryId")
 	public void handleProcessempinfoEvent(javax.portlet.EventRequest request,
@@ -28,6 +57,10 @@ public class InstanceListPortlet extends MVCPortlet {
 
 		response.setRenderParameter("categoryId", value);
 	}
-	
+
 	public static final String PARAM_CATEGORY_ID = "categoryId";
+
+	public static final String ATTR_CATEGORY_0 = "category0";
+	public static final String ATTR_CATEGORY_1 = "category1";
+
 }
